240f6a00b1ccd22463d2ad3c2edd9aa5f5b4859d
emher <emher@vestas.com>
2020-02-13 13:49:04 +0100

    First pypi release pushed

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import json
 import os
+import pathlib
+
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 with open(os.path.join('dash_leaflet', 'package.json')) as f:
     package = json.load(f)
@@ -16,5 +23,8 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package['description'] if 'description' in package else package_name,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thedirtyfew/dash-leaflet",
     install_requires=[]
 )
