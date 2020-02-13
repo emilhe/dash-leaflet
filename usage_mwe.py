2fc86bac3b661cbb1091222d0d6a0c8d2fff764d
emher <emher@vestas.com>
2020-02-13 12:21:06 +0100

    MWE seems to be running

---
+++ usage_mwe.py
@@ -0,0 +1,13 @@
+import dash
+import dash_leaflet as dl
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    dl.Map(style={'width': '1000px', 'height': '500px'}, center=[56.16, 10.21], zoom=10, children=[
+        dl.TileLayer(url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port=8051)
