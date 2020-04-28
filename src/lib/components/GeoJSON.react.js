12f1bed8cd360c22636aabef30331016263602c6
emher <emher@vestas.com>
2020-04-28 13:16:40 +0200

    GeoJSON is now working with style injection and zoom-on-click

--- src/lib/components/GeoJSON.react.js
+++ src/lib/components/GeoJSON.react.js
@@ -1,27 +1,24 @@
 import React, {Component, Node} from 'react';
 import PropTypes from 'prop-types';
 
-import { GeoJSON as LeafletGeoJSON } from 'react-leaflet';
+import {GeoJSON as LeafletGeoJSON} from 'react-leaflet';
+import {withLeaflet} from "react-leaflet";
 
 /**
  * LayerGroup is a wrapper of LayerGroup in react-leaflet.
  * It takes similar properties to its react-leaflet counterpart.
  */
-export default class GeoJSON extends Component {
+class GeoJSON extends Component {
+
     render() {
         const nProps = Object.assign({}, this.props);
+        const { map } = this.props.leaflet;
 
         // Read feature style information from style property if it exists.
         nProps.style = (feature) => {
-            if ('style' in feature)
-                return feature.style
-        } ;
-
-        // nProps.onEachFeature = (feature, layer) => {
-        //     layer.on({
-        //         click: (e) => map.fitBounds(e.target.getBounds());
-        //     });
-        // }
+              if (feature.style)
+                  return feature.style
+        };
 
 //         function onEachFeature(feature, layer) {
 //     layer.on({
@@ -30,16 +27,22 @@ export default class GeoJSON extends Component {
 //         click: zoomToFeature
 //     });
 // }
+        console.log(this.props);
+        console.log(map);
 
-        // // eslint-disable-next-line
-        // nProps.onclick = (e) => {
-        //     nProps.setProps({ click_lat_lng: [e.latlng.lat, e.latlng.lng] });
-        // }
+        // Attach event handlers. TODO: Which event handlers?
+        function onEachFeature(feature, layer) {
+            layer.on({
+                click: (e) => map.fitBounds(e.target.getBounds())
+            });
+            // layer.onclick = (e) => map.fitBounds(e.target.getBounds());
+          // // does this feature have a property named popupContent?
+          // if (feature.properties && feature.properties.popupContent) {
+          //   layer.bindPopup(feature.properties.popupContent);
+          // }
+        }
 
-        // // eslint-disable-next-line
-        // nProps.ondblclick = (e) => {
-        //     nProps.setProps({ dbl_click_lat_lng: [e.latlng.lat, e.latlng.lng] });
-        // }
+        nProps.onEachFeature = onEachFeature;
 
         // We need to use the non-JSX syntax to avoid having to list all props
         const el = React.createElement(
@@ -50,6 +53,7 @@ export default class GeoJSON extends Component {
 
         return el
     }
+
 }
 
 GeoJSON.propTypes = {
@@ -84,3 +88,4 @@ GeoJSON.propTypes = {
 
 }
 
+export default withLeaflet(GeoJSON);
