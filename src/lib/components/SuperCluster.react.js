5919c309f06aa4e25303df7b493c63e62f41082a
emher <emil.h.eriksen@gmail.com>
2020-07-13 13:43:33 +0200

    First semi-working version of supercluster

--- src/lib/components/SuperCluster.react.js
+++ src/lib/components/SuperCluster.react.js
@@ -1,9 +1,8 @@
-import React, {Component, Node} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-import LeafletSuperCluster from '../LeafletSuperCluster';
-import {withLeaflet} from "react-leaflet";
-// import Supercluster from 'supercluster';
+import { withLeaflet } from "react-leaflet";
+import Supercluster from 'supercluster';
+import { GeoJSON } from 'leaflet'
 
 /**
  * LayerGroup is a wrapper of LayerGroup in react-leaflet.
@@ -11,65 +10,86 @@ import {withLeaflet} from "react-leaflet";
  */
 class SuperCluster extends Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     // Create index.
-    //     const index = new Supercluster({
-    //         radius: 60,
-    //         extent: 256,
-    //         maxZoom: 18
-    //       })
-    //     index.load(props.data.features);
-    //     this.index = index;
-    //     // Create reference to be used for geojson object
-    //     this.myRef = React.createRef(); 
-    // }
+    componentDidMount() {
+        const { map } = this.props.leaflet;
+        const asyncfunc = async () => {
+            let data = this.props.data 
+            // Download data if needed.
+            if (~data.features) {
+                const response = await fetch(data);
+                data = await response.json();
+            }
+            // Create index.
+            const index = new Supercluster({
+                radius: 60,
+                extent: 256,
+                maxZoom: 18
+            })
+            index.load(data.features);
+            this.index = index;
+            // Create markers container.
+            const markers = new GeoJSON(null, {
+                pointToLayer: this._defaultCreateClusterIcon
+            });
 
-    // componentDidMount() {
-    //     // Add component to map.
-    //     const { map } = this.props.leaflet;
-    //     console.log(map)
-    //     map.options.onViewportChanged = (e) => {
-    //         console.log(e)
-    //     }
-    //   }
-    
-      render() {
-        return <LeafletSuperCluster {...this.props} />
-      }
+            function update() {
+                // Get map state.
+                const bounds = map.getBounds()
+                const zoom = map.getZoom()
+                const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
+                // Update the data.
+                var clusters = index.getClusters(bbox, zoom);
+                markers.clearLayers();
+                markers.addData(clusters);
+            }
 
-    // render() {
-    //     let nProps = Object.assign({}, this.props);
-    //     const {map} = this.props.leaflet;
-    //     // Get map state.
-    //     const bounds = map.getBounds()
-    //     const zoom = map.getZoom()
-    //     const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
-    //     // Update the data.
-    //     nProps.data = this.index.getClusters(bbox, zoom);
-    //     nProps.pointToLayer = this.createClusterIcon;
-    //     const el = <LeafletGeoJSON {...nProps} ref={this.myRef}/>
+            // Do initial update.
+            update();
+            // Bind component to map.
+            markers.addTo(map)
+            // Bind update.
+            map.on('moveend', update);
+            // Bind click-to-zoom.
+            markers.on('click', function (e) {
+                var clusterId = e.layer.feature.properties.cluster_id;
+                var center = e.latlng;
+                var expansionZoom;
+                if (clusterId) {
+                    expansionZoom = index.getClusterExpansionZoom(clusterId);
+                    map.flyTo(center, expansionZoom);
+                }
+            })
+        }
 
-    //     map.onViewportChanged = (e) => {
-    //         console.log(e)
-    //     }
-    //     // map.on('moveend', update) = (e) => {
-    //     //     console.log("hest")
-    //     // }
-    //     map.onmoveend = (e) => {
-    //         console.log("hest2")
-    //     }
-    //     console.log("hat")
-    //     return el
-    // }
+        asyncfunc();
+    }
 
+    render() {
+        return null;
+    }
+
+    _defaultCreateClusterIcon(feature, latlng) {
+        if (!feature.properties.cluster) return L.marker(latlng);
+
+        var count = feature.properties.point_count;
+        var size =
+            count < 100 ? 'small' :
+                count < 1000 ? 'medium' : 'large';
+        var icon = L.divIcon({
+            html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
+            className: 'marker-cluster marker-cluster-' + size,
+            iconSize: L.point(40, 40)
+        });
+
+        return L.marker(latlng, {
+            icon: icon
+        });
+    }
 
 
 }
 
 SuperCluster.defaultProps = {
-    featureId: "id",
-    n_clicks: 0
 };
 
 SuperCluster.propTypes = {
@@ -77,12 +97,7 @@ SuperCluster.propTypes = {
     /**
      * GeoJSON data
      */
-    data: PropTypes.object,
-
-    /**
-     * Attribution
-     */
-    attribution: PropTypes.string,
+    data: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 
     /**
      * Attribution
@@ -104,66 +119,8 @@ SuperCluster.propTypes = {
      */
     style: PropTypes.object,
 
-    /**
-     * Interactivity to be applied across all features.
-     */
-    defaultOptions: PropTypes.shape({
-        // Style to apply on mouse hover.
-        hoverStyle: PropTypes.object,
-        // If true, map will zoom to feature on click.
-        zoomToBoundsOnClick: PropTypes.bool,
-        // If set, a popup will be created on the feature with this content.
-        popupContent: PropTypes.string
-    }),
-
-    /**
-     * Style to be applied across all features.
-     */
-    defaultStyle: PropTypes.object,
-
-    /**
-     * Interactivity to be applied per feature (an id must be assigned to target a feature).
-     */
-    featureOptions: PropTypes.shape({
-        id: {
-            // Style to apply on mouse hover.
-            hoverStyle: PropTypes.object,
-            // If true, map will zoom to feature on click.
-            zoomToBoundsOnClick: PropTypes.bool,
-            // If set, a popup will be created on the feature with this content.
-            popupContent: PropTypes.string
-        }
-    }),
-
-    /**
-     * Style to be applied per feature (an id must be assigned to target a feature).
-     */
-    featureStyle: PropTypes.shape({
-        id: PropTypes.object,
-    }),
-
-    /**
-     * Which feature property to be used for matching as the feature id.
-     */
-    featureId: PropTypes.string,
-
     // Events
     setProps: PropTypes.func,
-
-    /**
-     * Dash callback property. Number of times the marker has been clicked
-     */
-    n_clicks: PropTypes.number,
-
-    /**
-     * Last feature clicked.
-     */
-    featureClick: PropTypes.object,
-
-    /**
-     * Last feature hover.
-     */
-    featureHover: PropTypes.object,
 
 };
 
