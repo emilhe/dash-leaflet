e14d0c7dbdff9eb977d82ea41d3648d93ae2afbf
emher <emil.h.eriksen@gmail.com>
2020-07-14 12:56:38 +0200

    Refactoring of super cluster into a react and a dash file

--- src/lib/LeafletSuperCluster.js
+++ src/lib/LeafletSuperCluster.js
@@ -1,77 +1,130 @@
-import {GeoJSON, ImageOverlay, Util, FeatureGroup} from 'leaflet'
+import { GeoJSON } from 'leaflet'
 import { withLeaflet, MapLayer } from 'react-leaflet';
 import Supercluster from 'supercluster';
-
-const LSuperCluster = FeatureGroup.extend({
-	options: {
-    createClusterIcon: null,
-    },
-
-    initialize: function (features, options) {
-      Util.setOptions(this, options);
-      // Set defaults.
-      if (!this.options.createClusterIcon) {
-        this.options.createClusterIcon = this._defaultCreateClusterIcon;
-      }
-        // Create index.
-        const index = new Supercluster({
-          radius: 60,
-          extent: 256,
-          maxZoom: 18
-        })
-        index.load(features);
-        this._index = index;
-        // Create markers container.
-        const markers = new GeoJSON(null, {
-          pointToLayer: this.options.createClusterIcon
-        });
-        this._markers = markers;
-        // Do initial update.
-        console.log(this._map)
-        this._update()
-        markers.addTo(this._map)
-        // Bind events.
-
-        // this._map.on('moveend', _update, this);
-    },
-    
-    _update: function () {
-        // Get map state.
-        const bounds = this._map.getBounds()
-        const zoom = this._map.getZoom()
-        const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
-        // Get the data.
-        const clusters = this._index.getClusters(bbox, zoom);
-        this._markers.clearLayers();
-        this._markers.addData(clusters);
-    },
-
-    _defaultCreateClusterIcon(feature, latlng) {
-      if (!feature.properties.cluster) return L.marker(latlng);
-    
-      var count = feature.properties.point_count;
-      var size =
-        count < 100 ? 'small' :
-        count < 1000 ? 'medium' : 'large';
-      var icon = L.divIcon({
-        html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
-        className: 'marker-cluster marker-cluster-' + size,
-        iconSize: L.point(40, 40)
-      });
-    
-      return L.marker(latlng, {
-        icon: icon
-      });
-    }
-});
+import {decode} from 'geobuf'
 
 class LeafletSuperCluster extends MapLayer {
 
+  componentDidMount() {
+    // Call super.
+    super.componentDidMount()
+    // Mount component.
+    const { map } = this.props.leaflet;
+    const { zoomToBoundsOnClick, options, format, url, data} = this.props;
+    const { leafletElement} = this;
+    let index;
+
+    function update() {
+      // Get map state.
+      const bounds = map.getBounds()
+      const zoom = map.getZoom()
+      const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
+      // Update the data.
+      var clusters = index.getClusters(bbox, zoom);
+      leafletElement.clearLayers();
+      leafletElement.addData(clusters);
+    }
+
+    function handleClick(e) {
+      var clusterId = e.layer.feature.properties.cluster_id;
+      var center = e.latlng;
+      var expansionZoom;
+      // Zoom to bounds on cluster click.
+      if (zoomToBoundsOnClick && clusterId) {
+        expansionZoom = index.getClusterExpansionZoom(clusterId);
+        map.flyTo(center, expansionZoom);
+      }
+    }
+
+    // Fetch data.
+    const asyncfunc = async () => {
+      let geojson = data
+      // Download data if needed.
+      if (!data && url) {
+        const response = await fetch(url);
+        if (format == "geojson") {
+          geojson = await response.json();
+        }
+        if (format == "geobuf") {
+          geojson = await response.arrayBuffer();
+        }
+      }
+      // Do any data transformations needed to arrive at geojson data.
+      if (format === "geobuf") {
+        var Pbf = require('pbf');
+        geojson = decode(new Pbf(geojson));
+      }
+      // Add cluster properties if they are missing.
+      geojson.features = geojson.features.map(feature => {
+        if (!feature.properties) {
+          feature["properties"] = {}
+        }
+        if (!feature.properties.cluster) {
+          feature["properties"]["cluster"] = false
+        }
+        return feature
+      })
+      // Create index.
+      index = new Supercluster(options)
+      index.load(geojson.features);
+      // Do initial update.
+      update();
+      // Bind update on map move (this is where the "magic" happens).
+      map.on('moveend', update);
+    }
+
+    // Load data.
+    asyncfunc();
+    // Bind click event(s).
+    this.leafletElement.on('click', handleClick)
+  }
+
+  componentWillUnmount(){
+    // Remove manually added event handlers.
+    map.on('moveend', update);
+    this.leafletElement.off('click', handleClick)
+    // Call super.
+    super.componentWillUnmount();
+  }
+
   createLeafletElement(props) {
-    return new LSuperCluster(props.data.features, {})
+    return new GeoJSON(null, {pointToLayer: this._defaultCreateClusterIcon});
   }
 
   updateLeafletElement(fromProps, toProps) {
+    // TODO: Implement this.
+  }
+
+  _defaultCreateClusterIcon(feature, latlng) {
+    if (!feature.properties.cluster) {
+      const marker_options = feature.properties.marker_options ? feature.properties.marker_options : {}
+      const marker = L.marker(latlng, marker_options);
+      // Add tooltip if present in feature properties.
+      if (feature.properties.tooltip) {
+        const tooltip_options = feature.properties.tooltip_options ? feature.properties.tooltip_options : {}
+        marker.bindTooltip(feature.properties.tooltip, tooltip_options)
+      }
+      // Add popup if present in feature properties.
+      if (feature.properties.popup) {
+        const popup_options = feature.properties.popup_options ? feature.properties.popup_options : {}
+        marker.bindPopup(feature.properties.popup, popup_options)
+      }
+      return marker;
+    }
+
+    var count = feature.properties.point_count;
+    var size =
+      count < 100 ? 'small' :
+        count < 1000 ? 'medium' : 'large';
+    var icon = L.divIcon({
+      html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
+      className: 'marker-cluster marker-cluster-' + size,
+      iconSize: L.point(40, 40)
+    });
+
+    return L.marker(latlng, {
+      icon: icon
+    });
   }
 
 }
--- src/lib/components/SuperCluster.react.js
+++ src/lib/components/SuperCluster.react.js
@@ -1,9 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withLeaflet } from "react-leaflet";
-import Supercluster from 'supercluster';
-import { GeoJSON } from 'leaflet'
-import {decode} from 'geobuf'
+import { withLeaflet} from "react-leaflet";
+import LeafletSuperCluster from '../LeafletSuperCluster';
 
 /**
  * LayerGroup is a wrapper of LayerGroup in react-leaflet.
@@ -11,107 +9,16 @@ import {decode} from 'geobuf'
  */
 class SuperCluster extends Component {
 
-    componentDidMount() {
-        const nProps = Object.assign({}, this.props);
-        const { map } = nProps.leaflet;
-        const asyncfunc = async () => {
-            let data = nProps.data
-            // Download data if needed.
-            if (!data && nProps.url) {
-                const response = await fetch(nProps.url);
-                if(nProps.format == "geojson"){
-                    data = await response.json();
-                }
-                if(nProps.format == "geobuf"){
-                    data = await response.arrayBuffer();
-                }
-            }
-            // Do any data transformations needed to arrive at geojson data.
-            if(nProps.format === "geobuf"){
-                var Pbf = require('pbf');
-                data = decode(new Pbf(data));
-            }
-            // Add cluster properties if they are missing.
-            data.features = data.features.map(feature => {
-                if(!feature.properties){
-                    feature["properties"] = {}
-                }
-                if(!feature.properties.cluster){
-                    feature["properties"]["cluster"] = false
-                }
-                return feature
-            })
-
-            // Create index.
-            const index = new Supercluster(nProps.options)
-            index.load(data.features);
-            this.index = index;
-            // Create markers container.
-            const markers = new GeoJSON(null, {
-                pointToLayer: this._defaultCreateClusterIcon
-            });
-
-            function update() {
-                // Get map state.
-                const bounds = map.getBounds()
-                const zoom = map.getZoom()
-                const bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()]
-                // Update the data.
-                var clusters = index.getClusters(bbox, zoom);
-                markers.clearLayers();  
-                markers.addData(clusters);
-            }
-
-            function handleClick(e){
-                var clusterId = e.layer.feature.properties.cluster_id;
-                var center = e.latlng;
-                var expansionZoom;
-                // Zoom to bounds on cluster click.
-                if (nProps.zoomToBoundsOnClick && clusterId) {
-                    expansionZoom = index.getClusterExpansionZoom(clusterId);
-                    map.flyTo(center, expansionZoom);
-                }
-                // Dash events.
-                nProps.n_clicks = nProps.n_clicks + 1
-                nProps.setProps({n_clicks: nProps.n_clicks});
-                nProps.setProps({marker_click: e.layer.feature})
-            }
-
-            // Do initial update.
-            update();
-            // Bind component to map.
-            markers.addTo(map)
-            // Bind update on map move (this is where the "magic" happens).
-            map.on('moveend', update);
-            // Bind click event(s).
-            markers.on('click', handleClick)
-        }
-
-        asyncfunc();
-    }
-
     render() {
-        return null;
-    }
-
-    _defaultCreateClusterIcon(feature, latlng) {
-        if (!feature.properties.cluster) return L.marker(latlng);
-
-        var count = feature.properties.point_count;
-        var size =
-            count < 100 ? 'small' :
-                count < 1000 ? 'medium' : 'large';
-        var icon = L.divIcon({
-            html: '<div><span>' + feature.properties.point_count_abbreviated + '</span></div>',
-            className: 'marker-cluster marker-cluster-' + size,
-            iconSize: L.point(40, 40)
-        });
-
-        return L.marker(latlng, {
-            icon: icon
-        });
-    }
-
+        const nProps = Object.assign({}, this.props);
+        // Bind events.
+        nProps.onclick = (e) => {
+            nProps.setProps({ n_clicks: nProps.n_clicks + 1 });
+            nProps.setProps({ marker_click: e.layer.feature});
+        }
+        // Render the leaflet component.
+        return <LeafletSuperCluster {...nProps}/>
+      }
 
 }
 
@@ -138,7 +45,6 @@ SuperCluster.propTypes = {
      * Data format.
      */
     format: PropTypes.oneOf(["geojson", "geobuf"]),
-
 
     /**
      * Options passed to SuperCluster, https://github.com/mapbox/supercluster.
