f2b776271bbd713831a765a74993d4e615c551e4
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2021-03-12 23:44:00 +0000

    add LeafletDraw

---
+++ src/lib/LeafletDraw.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import L from 'leaflet';
+import { MapControl, withLeaflet } from 'react-leaflet';
+
+require('leaflet-draw')
+require('leaflet-draw/dist/leaflet-draw.css')
+
+export class LeafletDraw extends MapControl {
+
+  createLeafletElement(props) {
+    const el = new L.Control.Draw(props);
+    this.contextValue = {
+      ...props.leaflet,
+      layerContainer: el
+    };
+    return el;
+  }
+
+}
+
+export default withLeaflet(LeafletDraw)
