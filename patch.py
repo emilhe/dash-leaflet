b23816294fa031dd31f26c303ce11d7fb476c82b
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2023-08-25 08:13:39 +0200

    Bump Dash dependency

--- patch.py
+++ patch.py
@@ -1,14 +1,5 @@
-import os
-import sys
+"""
+This file holds any necessary Python patches.
+"""
 
-print(os.listdir())
-print(sys.executable)
-
-target = str(sys.executable).replace("bin/python", "lib/python3.10/site-packages/dash/extract-meta.js")
-current = "JSON.parse(fs.readFileSync('tsconfig.json')).compilerOptions;"
-fix = "JSON.parse(fs.readFileSync('tsconfig.json')).compilerOptions;\ntsconfig.moduleResolution=tsconfig.moduleResolution == 'node' ? ts.ModuleResolutionKind.NodeJs : tsconfig.moduleResolution;"
-with open(target, 'r') as f:
-    content = f.read()
-with open(target, 'w') as f:
-    fixed = content.replace(current, fix)
-    f.write(fixed)
+print("Hello from the Python patch script!")
