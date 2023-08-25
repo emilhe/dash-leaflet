44ce561b69ab69be68caf13fd8cc7bb419a639a9
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2023-08-25 10:14:07 +0200

    Try re-add desc

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import json
+import os
+import pathlib
+
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-with open('package.json') as f:
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+with open(os.path.join(HERE, 'package.json')) as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
@@ -14,7 +22,10 @@ setup(
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
-    description=package.get('description', package_name),
+    description=package['description'] if 'description' in package else package_name,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thedirtyfew/dash-leaflet",
     install_requires=[],
     classifiers=[
         'Framework :: Dash',
