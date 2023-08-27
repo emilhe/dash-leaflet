a32040af2828176b6d21b9181ff4f6b17f05b25f
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2023-08-27 16:34:11 +0200

    Add map viewport tracking

--- src/ts/components/MapContainer.tsx
+++ src/ts/components/MapContainer.tsx
@@ -5,12 +5,30 @@ import {resolveCRS, resolveEventHandlers, resolveRenderer} from '../utils';
 import '../../../node_modules/leaflet/dist/leaflet.css';
 import {ClickEvents, KeyboardEvents, LoadEvents, MapContainerProps, DashComponent, Modify} from "../props";
 
+const trackViewport = (map, props) => {
+    const bounds = map.getBounds()
+    props.setProps({
+        zoom: map.zoom, center: map.center,
+        bounds: [[bounds.getSouth(), bounds.getWest()], [bounds.getNorth(), bounds.getEast()]]
+    })
+}
+
 function EventSubscriber(props) {
-    const map = useMapEvents(resolveEventHandlers(props, ["click", "dblclick", "keydown", "load"]))
+    const eventHandlers = resolveEventHandlers(props, ["click", "dblclick", "keydown", "load"])
+    const map = useMapEvents(Object.assign(eventHandlers, !props.trackViewport? {} : {
+        moveend: (e) => {
+            trackViewport(map, props);
+        }
+    }));
+    if(props.trackViewport) {
+        map.whenReady(() => {
+            trackViewport(map, props);
+        })
+    }
 
     useEffect(function invalidateSize(){
         if(props.invalidateSize !== undefined){
-            map.invalidateSize()
+            map.invalidateSize();
         }
     }, [props.invalidateSize])
 
@@ -34,14 +52,23 @@ type Props = Modify<MapContainerProps, {
     /**
      * Change the value to force map size invalidation. [DL]
      */
-    invalidateSize?: string | number | object
+    invalidateSize?: string | number | object;
+
+    /**
+     * If true (default), zoom, center, and bounds properties are updated on whenReady/moveend. [DL]
+     */
+    trackViewport?: boolean;
 }  & DashComponent & ClickEvents & LoadEvents & KeyboardEvents>;
 
 /**
- * Component description
+ * The MapContainer component is responsible for creating the Leaflet Map instance and providing it to its child components, using a React Context.
  */
-const MapContainer = ({crs="EPSG3857", renderer=undefined, ...props}: Props) => {
-    const target = {crs: resolveCRS(crs), renderer: resolveRenderer(renderer)}  // map from string repr of CRS to actual object
+const MapContainer = ({crs="EPSG3857", renderer=undefined, trackViewport=true, ...props}: Props) => {
+    const target = {
+        crs: resolveCRS(crs), // map from string repr of CRS to actual object
+        renderer: resolveRenderer(renderer),  // map from string repr of Renderer to actual object
+        trackViewport: trackViewport
+    }
     const nProps = Object.assign(target, props);
     // Add a custom event subscriber that exposes events to Dash.
     return (
