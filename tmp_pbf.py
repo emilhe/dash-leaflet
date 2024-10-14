31a9d056b28b0e90625730e3d261dab5d63c7b2b
emilhe <emil.h.eriksen@gmail.com>
2024-10-14 19:45:25 +0200

    Update example code

--- tmp_pbf.py
+++ tmp_pbf.py
@@ -1,11 +1,14 @@
 import dash_leaflet as dl
 from dash import Dash
+from dash_extensions.javascript import assign
+
+eventHandlers = dict(click=assign("function(e, ctx){console.log(e); console.log(ctx);}"))
 
 app = Dash()
 app.layout = dl.Map(
     [
         dl.TileLayer(),
-        dl.VectorTileLayer(url="https://openinframap.org/tiles/{z}/{x}/{y}.pbf", maxDetailZoom=6, style={}),
+        dl.VectorTileLayer(url="https://openinframap.org/tiles/{z}/{x}/{y}.pbf", maxDetailZoom=6, style={}, eventHandlers=eventHandlers),
     ],
     center=[56, 10],
     zoom=8,
