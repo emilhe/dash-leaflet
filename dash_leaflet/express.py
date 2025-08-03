d61631a3861c97e1e7816216d312fa39f043f2c4
Adam Reeder <areed145@gmail.com>
2025-08-03 11:35:31 +0100

    Update express.py

--- dash_leaflet/express.py
+++ dash_leaflet/express.py
@@ -12,7 +12,7 @@ def _try_import_geobuf():
     except ImportError as ex:
         logging.error(f"Unable to import [geobuf/protobuf]. Please install it, {install_txt}")
         raise ex
-    if google.protobuf.__version__ >= "6.30.2":
+    if google.protobuf.__version__ < "6.30.2":
         version_txt = f"The recommended protobuf version is at least 6.30.2 (you have {google.protobuf.__version__})"
         logging.warning(f"{version_txt}. You can fix it {install_txt}")
     return geobuf
